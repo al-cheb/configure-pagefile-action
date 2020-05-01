722cd6e0925690b543b2d3399a12e740bbbc07a8
Maxim Lobanov <v-malob@microsoft.com>
2020-05-01 21:29:51 +0300

    debug arguments

--- src/configure-pagefile.ts
+++ src/configure-pagefile.ts
@@ -27,7 +27,7 @@ const run = (): void => {
         core.debug(`Script path: ${scriptPath}`);
         core.debug(`Script arguments: ${scriptArguments}`);
 
-        const scriptResult = child.spawnSync("powershell", ["-File", scriptPath, ...scriptArguments], {
+        const scriptResult = child.spawnSync("pwsh", ["-File", scriptPath], {
             timeout: 60 * 1000
         });
         core.info(JSON.stringify(scriptResult, null, 2));
