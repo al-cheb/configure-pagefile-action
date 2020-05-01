264190f397d8bc7f513275bcc4ec3269bdbf6f26
Maxim Lobanov <v-malob@microsoft.com>
2020-05-01 19:32:08 +0300

    Implement first version of action

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # configure-pagefile-action
-Set PageFileSize on GitHub Windows Hosted Agents
+This action is intended to configure Pagefile size and location for Windows images in GitHub Actions.  
+
+# Available parameters
+| Argument                | Description              | Default value      |
+|-------------------------|--------------------------|--------------------|
+| `minimum-size`          | Set minimum size of Pagefile (GB) | `8` |
+| `maximum-size`          | Set maximum size of Pagefile (GB) | `minimum-size` |
+| `disk-root`          | Set disk root where Pagefile will be located | `C:`, `D:` |
+
+# Usage
+```
+name: CI
+on: [push]
+jobs:
+  build:
+    runs-on: windows-latest
+    steps:
+    - name: configure Pagefile
+      uses: al-cheb/configure-pagefile-action@v1.0
+      with:
+        minimum-size: 8
+
+    - name: configure Pagefile
+      uses: al-cheb/configure-pagefile-action@v1.0
+      with:
+        minimum-size: 8
+        maximum-size: 16
+        disk-root: "D:"
+```
+
+# License
+The scripts and documentation in this project are released under the [MIT License](LICENSE)
---
+++ src/configure-pagefile.ts
@@ -0,0 +1,48 @@
+import * as path from "path";
+import * as child from "child_process";
+import * as core from "@actions/core";
+
+const getSizeInput = (inputName: string, defaultValue?: number): number => {
+    const inputValue = core.getInput(inputName, { required: !defaultValue });
+    if (defaultValue && !inputValue) {
+        return defaultValue;
+    }
+
+    let size = parseInt(inputValue);
+    if (isNaN(size)) {
+        throw new Error(`Unable to parse input '${inputName}'. Value '${inputValue}' is invalid.`)
+    }
+
+    // convert GB to Byte
+    return size * 1024 * 1024 * 1024;
+}
+
+const run = (): void => {
+    try {
+        if (process.platform !== "win32") {
+            throw new Error(`This task is intended only for Windows platform. It can't be run on '${process.platform}' platform`);
+        }
+
+        const minimumSize = getSizeInput("minimum-size");
+        const maximumSize = getSizeInput("maximum-size", minimumSize);
+        const diskRoot = core.getInput("disk-root", { required: true });
+
+        core.info("Pagefile configuration:");
+        core.info(`- Minimum size: ${minimumSize}`);
+        core.info(`- Maximum size: ${maximumSize}`);
+        core.info(`- Disk root: ${diskRoot}`);
+
+        const scriptPath = path.resolve(path.join("..", "scripts", "SetPageFileSize.ps1"));
+        const scriptArguments = [minimumSize, maximumSize, diskRoot].map(String);
+        core.debug("Invoke configuration script:");
+        core.debug(`Script path: ${scriptPath}`);
+        core.debug(`Script arguments: ${scriptArguments}`)
+        child.execFileSync(scriptPath, scriptArguments, {
+            timeout: 60 * 1000 // 60 seconds
+        });
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+};
+
+run();
